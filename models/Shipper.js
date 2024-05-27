980666b04a86051dc30fd412acb9df6316203c99
gemmen29 <georgtriple@gmail.com>
2024-05-27 12:02:15 +0300

    Refactor shipper model

--- models/Shipper.js
+++ models/Shipper.js
@@ -14,6 +14,10 @@ const ShipperSchema = new mongoose.Schema(
       type: String,
       required: [true, 'Please provide a motorcycle number'],
     },
+    suspended: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );
