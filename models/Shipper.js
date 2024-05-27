64192a7eb73df6a6b9754d551df8f868694492fc
gemmen29 <georgtriple@gmail.com>
2024-05-27 12:00:32 +0300

    Add Shipper model

---
+++ models/Shipper.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const ShipperSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide a name'],
+    },
+    uid: {
+      type: String,
+      required: [true, 'Please provide a uid'],
+    },
+    motorcycleNumber: {
+      type: String,
+      required: [true, 'Please provide a motorcycle number'],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Shipper', ShipperSchema);
