a8fbf1ae0bc879d4b2b149d2c3de08383475b5b0
gemmen29 <georgtriple@gmail.com>
2024-05-27 13:36:03 +0300

    Add getSingleOrder action

--- controllers/order.js
+++ controllers/order.js
@@ -12,7 +12,14 @@ const createOrder = async (req, res) => {
 };
 
 const getSingleOrder = async (req, res) => {
-  res.send('Get single order');
+  const { id: orderID } = req.params;
+  const order = await Order.findOne({ _id: orderID });
+
+  if (!order) {
+    throw new CustomError.NotFoundError(`No order with id: ${orderID}`);
+  }
+
+  res.status(StatusCodes.OK).json(order);
 };
 
 const updateOrder = async (req, res) => {
