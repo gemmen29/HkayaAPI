3b625b02c4612d67224ed49613fe9f293d5b8f2e
gemmen29 <georgtriple@gmail.com>
2024-05-27 19:01:31 +0300

    Refactor pagination util

--- utils/pagination.js
+++ utils/pagination.js
@@ -1,9 +1,9 @@
-const pagination = async (model, req) => {
+const pagination = async (model, req, queryObj = {}) => {
   const page = parseInt(req.query.page) || 1;
   const limit = parseInt(req.query.limit) || 10;
   const skip = (page - 1) * limit;
 
-  const totalMatches = await model.countDocuments();
+  const totalMatches = await model.countDocuments(queryObj);
   const numOfPages = Math.ceil(totalMatches / limit);
 
   return { skip, limit, totalMatches, numOfPages };
