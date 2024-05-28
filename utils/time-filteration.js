b330faff672715d42a1746459f1fde102babfecc
gemmen29 <georgtriple@gmail.com>
2024-05-28 10:56:33 +0300

    Add timeFilteration helper

---
+++ utils/time-filteration.js
@@ -0,0 +1,12 @@
+const timeFilteration = (from, to) => {
+  const startDate = new Date(from ?? Date.now());
+  const endDate = new Date(to ?? Date.now());
+  startDate.setHours(0, 0, 0, 0);
+  endDate.setHours(23, 59, 59, 999);
+
+  console.log(startDate.toLocaleString(), endDate.toLocaleString());
+
+  return { startDate, endDate };
+};
+
+module.exports = timeFilteration;
